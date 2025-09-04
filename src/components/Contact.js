bbdacbfc1a339f094cf59af27447ba194a770c86
SanidhyaPanwar <sanidhyapanwar1@gmail.com>
2025-09-04 21:09:40 +0530

    Added contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,53 +1,109 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
     <section className="contact" id="contact">
-        <div className="max-width">
-            <h2 className="title">Contact me</h2>
-            <div className="contact-content">
-                <div className="column left">
-                    <div className="text">Get in Touch</div>
-                    <p>
-                        Feel free to contact me for any queries.
-                    </p>
-                    <div className="icons">
-                        <div className="row">
-                            <i className="fas fa-user"></i>
-                            <div className="info">
-                                <div className="head">Name</div>
-                                <div className="sub-title">Sanidhya Panwar</div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <i className="fas fa-map-marker-alt"></i>
-                            <div className="info">
-                                <div className="head">Address</div>
-                                <div className="sub-title">Meerut, Uttar Pradesh</div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <i className="fas fa-envelope"></i>
-                            <div className="info">
-                                <div className="head">Email</div>
-                                <div className="sub-title">sanidhyapanwar1@gmail.com</div>
-                            </div>
-                        </div>
-                    </div>
+      <div className="max-width">
+        <h2 className="title">Contact Us</h2>
+        <div className="contact-content">
+          {/* Left Column */}
+          <div className="text">We'd love to hear from you!</div>
+            <p>
+              Whether you're curious about our crochet collections, want to place a custom order, or just want to say hi, feel free to reach out!
+            </p>
+            <div className="column left">
+            
+            <div className="icons">
+              <div className="row">
+                <i className="fas fa-user"></i>
+                <div className="info">
+                  <div className="head">Who we are</div>
+                  <div className="sub-title">Your Box Of Yarn (YBOY)</div>
                 </div>
-                <div className="column right">
-                    <div className="text">
-                        <q>
-                            Showcasing your talent through working projects is far better than adding just another certification in your resume.
-                        </q>
-                        <br/>
-                    </div>
-                    <cite>-Sanidhya Panwar</cite>
+              </div>
+              <div className="row">
+                <i className="fas fa-map-marker-alt"></i>
+                <div className="info">
+                  <div className="head">Location</div>
+                  <div className="sub-title">Jhansi, Uttar Pradesh, India 🇮🇳</div>
                 </div>
+              </div>
+              <div className="row">
+                <i className="fas fa-envelope"></i>
+                <div className="info">
+                  <div className="head">Email</div>
+                  <div className="sub-title">yboy@gmail.com</div>
+                </div>
+              </div>
+              {/* Optional: Add social media */}
+              <div className="row">
+                <i className="fab fa-instagram"></i>
+                <div className="info">
+                  <div className="head">Instagram</div>
+                  <div className="sub-title">
+                    <a
+                      href="https://instagram.com/yourboxofyarn"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      @yourboxofyarn
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <i className="fab fa-youtube"></i>
+                <div className="info">
+                  <div className="head">YouTube</div>
+                  <div className="sub-title">
+                    <a
+                      href="https://youtube.com/@yourboxofyarn"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      youtube.com/@yourboxofyarn
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    </section>
-  )
-}
+          </div>
 
-export default Contact
+          {/* Right Column */}
+          <div className="column right">
+            <div className="text">
+              <q>
+                There’s magic in turning yarn into something beautiful;
+                stitch by stitch, story by story.
+              </q>
+              <br />
+            </div>
+            <cite>- Your Box Of Yarn</cite>
+
+            {/* Optional: Add a contact form */}
+            {/* 
+            <form>
+              <div className="fields">
+                <div className="field name">
+                  <input type="text" placeholder="Your Name" required />
+                </div>
+                <div className="field email">
+                  <input type="email" placeholder="Your Email" required />
+                </div>
+              </div>
+              <div className="field">
+                <textarea placeholder="Your Message" required></textarea>
+              </div>
+              <div className="button">
+                <button type="submit">Send Message</button>
+              </div>
+            </form>
+            */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,8 +20,8 @@ const Navbar = () => {
                 <li><Link to="/education" className="menu-btn">Education</Link></li>
                 <li><Link to="/interests" className="menu-btn">Interests</Link></li>
                 <li><Link to="/skills" className="menu-btn">Skills</Link></li>
-                <li><Link to="/projects" className="menu-btn">Projects</Link></li>
-                <li><Link to="/contact" className="menu-btn">Contact</Link></li> */}
+                <li><Link to="/projects" className="menu-btn">Projects</Link></li>*/}
+                <li><Link to="/contact" className="menu-btn">Contact</Link></li> 
             </ul>
             <div className="menu-btn">
                 <i className="fas fa-bars"></i>
