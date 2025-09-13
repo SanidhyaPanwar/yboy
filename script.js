3da24d41a971a2ed6095406930d5093183eada22
SanidhyaPanwar <sanidhyapanwar1@gmail.com>
2025-09-13 20:57:14 +0530

    Updates

--- script.js
+++ script.js
@@ -1,56 +1,45 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-    const btn = document.querySelector(".btn-toggle");
-    // Listen for a click on the button
-    btn.addEventListener("click", function() {
-    // Then toggle (add/remove) the .dark-theme class to the body
-    document.body.classList.toggle("dark-theme");
-    });
-    //dark mode button toggle
-    $("#darkMode").click(function () {
-        $("#darkMode").toggleClass("fa-sun");
-    });
-    // const icon = document.querySelector(".fas fa-home");
-    // icon.addEventListener("click", function() {
-    //     icon.classList.toggle("fas fa-moon");
-    // }
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         } else {
+//             $('.navbar').removeClass("sticky");
+//         }
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+//         if(this.scrollY > 500){
+//             $('.scroll-up-btn').addClass("show");
+//         } else {
+//             $('.scroll-up-btn').removeClass("show");
+//         }
+//     });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-    
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Cute Keyrings", "cozy Knits","slow Fashion","Made-for-You Knits"],
-        typeSpeed: 120,
-        backSpeed: 60,
-        loop: true
-    });
-});
+//     // Dark mode button
+//     $("#darkMode, .btn-toggle").click(function() {
+//         $("body").toggleClass("dark-theme");
+//         $(this).toggleClass("fa-sun fa-moon"); // toggle icon
+//     });
+
+//     // Slide-up script
+//     $('.scroll-up-btn').click(function(){
+//         $('html').animate({scrollTop: 0});
+//         $('html').css("scrollBehavior", "auto");
+//     });
+
+//     $('.navbar .menu li a').click(function(){
+//         $('html').css("scrollBehavior", "smooth");
+//     });
+
+//     // Toggle menu/navbar script
+//     $('.menu-btn').click(function(){
+//         $('.navbar .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+//     });
+
+//     // Typing text animation
+//     var typed = new Typed(".typing", {
+//         strings: ["Cute Keyrings", "cozy Knits","slow Fashion","Made-for-You Knits"],
+//         typeSpeed: 120,
+//         backSpeed: 60,
+//         loop: true
+//     });
+// });
